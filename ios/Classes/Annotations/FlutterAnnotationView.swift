2ad670949e75e92622cb4ffa50fc5a47285701d3
Ax0elz <alex.kotliarsky@gmail.com>
2024-11-20 17:59:39 -0700

    requried disp priortiy

--- ios/Classes/Annotations/FlutterAnnotationView.swift
+++ ios/Classes/Annotations/FlutterAnnotationView.swift
@@ -8,74 +8,74 @@
 import Foundation
 import MapKit
 
-// protocol ZPositionableAnnotation {
-//     var stickyZPosition: CGFloat {
-//         get
-//         set
-//     }
-// }
+protocol ZPositionableAnnotation {
+    var stickyZPosition: CGFloat {
+        get
+        set
+    }
+}
 
 class FlutterAnnotationView: MKAnnotationView {
 
     /// Override the layer factory for this class to return a custom CALayer class
-    // override class var layerClass: AnyClass {
-    //     return ZPositionableLayer.self
-    // }
+    override class var layerClass: AnyClass {
+        return ZPositionableLayer.self
+    }
 
-    // /// convenience accessor for setting zPosition
-    // var stickyZPosition: CGFloat {
-    //     get {
-    //         return (self.layer as! ZPositionableLayer).stickyZPosition
-    //     }
-    //     set {
-    //         (self.layer as! ZPositionableLayer).stickyZPosition = newValue
-    //     }
-    // }
+    /// convenience accessor for setting zPosition
+    var stickyZPosition: CGFloat {
+        get {
+            return (self.layer as! ZPositionableLayer).stickyZPosition
+        }
+        set {
+            (self.layer as! ZPositionableLayer).stickyZPosition = newValue
+        }
+    }
 }
 
 @available(iOS 11.0, *)
 class FlutterMarkerAnnotationView: MKMarkerAnnotationView {
     /// Override the layer factory for this class to return a custom CALayer class
-    // override class var layerClass: AnyClass {
-    //     return ZPositionableLayer.self
-    // }
+    override class var layerClass: AnyClass {
+        return ZPositionableLayer.self
+    }
 }
 
-// @available(iOS 11.0, *)
-// extension FlutterMarkerAnnotationView: ZPositionableAnnotation {
-//     /// convenience accessor for setting zPosition
-//     // var stickyZPosition: CGFloat {
-//     //     get {
-//     //         return (self.layer as! ZPositionableLayer).stickyZPosition
-//     //     }
-//     //     set {
-//     //         (self.layer as! ZPositionableLayer).stickyZPosition = newValue
-//     //     }
-//     // }
-// }
+@available(iOS 11.0, *)
+extension FlutterMarkerAnnotationView: ZPositionableAnnotation {
+    /// convenience accessor for setting zPosition
+    var stickyZPosition: CGFloat {
+        get {
+            return (self.layer as! ZPositionableLayer).stickyZPosition
+        }
+        set {
+            (self.layer as! ZPositionableLayer).stickyZPosition = newValue
+        }
+    }
+}
 
 /// iOS 11 automagically manages the CALayer zPosition, which breaks manual z-ordering.
 /// This subclass just throws away any values which the OS sets for zPosition, and provides
 /// a specialized accessor for setting the zPosition
-// private class ZPositionableLayer: CALayer {
+private class ZPositionableLayer: CALayer {
 
-//     /// no-op accessor for setting the zPosition
-//     override var zPosition: CGFloat {
-//         get {
-//             return super.zPosition
-//         }
-//         set {
-//             // do nothing
-//         }
-//     }
+    /// no-op accessor for setting the zPosition
+    override var zPosition: CGFloat {
+        get {
+            return super.zPosition
+        }
+        set {
+            // do nothing
+        }
+    }
 
-//     /// specialized accessor for setting the zPosition
-//     var stickyZPosition: CGFloat {
-//         get {
-//             return super.zPosition
-//         }
-//         set {
-//             super.zPosition = newValue
-//         }
-//     }
-// }
+    /// specialized accessor for setting the zPosition
+    var stickyZPosition: CGFloat {
+        get {
+            return super.zPosition
+        }
+        set {
+            super.zPosition = newValue
+        }
+    }
+}
